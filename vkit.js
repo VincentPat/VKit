7dec520e921f5ae9827be6ab7de88fa967ee16ff
VincentPat <hys510573094@gmail.com>
2016-06-21 14:08:48 +0800

    v1.0.1 增加日期格式化方法

--- vkit.js
+++ vkit.js
@@ -1,6 +1,6 @@
 /**
  * @author VincentHuang
- * @version 1.0.0
+ * @version 1.0.1
  * @description 常用的一些方法，整理到工具包中
  */
 var VKit = function() {
@@ -68,6 +68,42 @@ var VKit = function() {
             m = (r2.split(".")[1] ? r2.split(".")[1].length : 0) - (r1.split(".")[1] ? r1.split(".")[1].length : 0);
             resultVal = Number(r1.replace(".", "")) / Number(r2.replace(".", "")) * Math.pow(10, m);
             return typeof d !== "number" ? Number(resultVal) : Number(resultVal.toFixed(parseInt(d)));
+		},
+
+		/**
+		 * 日期格式化
+		 * @param  {Date} date   日期
+		 * @param  {String} format 格式
+		 * @return {String}        格式化日期
+		 */
+		dateFormat: function(date, format) {
+		    if(format === undefined){
+		        format = date;
+		        date = new Date();
+		    }
+		    var map = {
+		        "M": date.getMonth() + 1, //月份 
+		        "d": date.getDate(), //日 
+		        "h": date.getHours(), //小时 
+		        "m": date.getMinutes(), //分 
+		        "s": date.getSeconds(), //秒 
+		        "q": Math.floor((date.getMonth() + 3) / 3), //季度 
+		        "S": date.getMilliseconds() //毫秒 
+		    };
+		    format = format.replace(/([yMdhmsqS])+/g, function(all, t){
+		        var v = map[t];
+		        if(v !== undefined){
+		            if (all.length > 1) {
+		                v = '0' + v;
+		                v = v.substr(v.length-2);
+		            }
+		            return v;
+		        } else if (t === 'y') {
+		            return (date.getFullYear() + '').substr(4 - all.length);
+		        }
+		        return all;
+		    });
+		    return format;
 		}
 
 	};
